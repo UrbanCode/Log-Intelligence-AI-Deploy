590940629a6ae1a3584776d1bc805d12ca7bd325
Garlapati Anil Masthan Setty <garlapatianil.masth@prod.hclpnp.com>
2026-08-07 11:10:52 +0530

    Add initial project structure

---
+++ src/main/java/com/logai/deploy/LogIntelligenceAiDeployApplication.java
@@ -0,0 +1,13 @@
+package com.logai.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogIntelligenceAiDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogIntelligenceAiDeployApplication.class, args);
+	}
+
+}
